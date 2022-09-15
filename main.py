c3776f4ee1d1a8796307e5431662c4f16c0098c3
arnaudontour <arnaud.lahousse@student.uclouvain.be>
2022-09-15 14:44:40 +0200

    Learning rate

--- main.py
+++ main.py
@@ -3,23 +3,39 @@ import random
 
 class perceptron :
     #construtor
-    def __init__(self, nb):
+    def __init__(self, nb,lr):
+
+        # learning rate
+        self.lr = lr
+
+        #setup weight with a random value
         self.weight = [p for p in range(0, nb)]
         for i in range(0,nb):
             self.weight[i] = random.random()
 
-    #train method
-    def train(self, set):
-        for i in range(0,len(self.weight)):
-            self.weight[i] =
 
     #result method
-    def result(self):
-        res =
-        for w in self.weight:
-            res 
+    def result(self,input):
+        res = 0
+        for i in range(0,len(self.weight)):
+            res += self.weight[i] * input[i]
+        if(res >= 0):
+            return 1
+        else :
+            return -1
 
-p = perceptron(6)
+    #train method
+    def train(self, set, label):
+        res = self.result(set)
+        err = label-res
+        for i in range(0,len(self.weight)):
+            self.weight[i] += err * set[i] * self.lr
+
+
+
+
+p = perceptron(2,1)
 print(p.weight)
-p.train()
+p.train([1,2],-1)
 print(p.weight)
+  j
